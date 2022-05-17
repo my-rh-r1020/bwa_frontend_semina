56bfc1d80bd893f70a075dd64480e011396a3253
my-rh-r1020 <rherikson.my@gmail.com>
2022-05-17 17:59:14 +0700

    Session 10 - React useEffect

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [categories, setCategories] = useState([]);
-
-  // console.log(tombol, tombol2);
+  // Use State
+  const [categories, setCategories] = useState([]),
+    [speakers, setSpeakers] = useState([]),
+    [keyword, setKeyword] = useState([]);
 
   // // Component Did
   // React.useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
   //   console.log("This use effect Did 2");
   // }, [tombol]);
 
+  // Get data categories from API
   const getAllCategories = async () => {
     const res = await axios.get("http://localhost:4000/api/v1/categories", {
       headers: {
@@ -26,24 +28,47 @@ function App() {
     setCategories(res.data.data);
   };
 
-  console.log(categories);
+  // Get data speakers from API
+  const getAllSpeaker = async () => {
+    const res = await axios.get(`http://localhost:4000/api/v1/speakers?keyword=${keyword}`, {
+      headers: {
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUmlmYWxkaSBIZXJpa3NvbiIsInVzZXJJZCI6IjYyNGZlZjgxNjMzYjY4ODk5NWYzMGJiMiIsInJvbGUiOiJzdXBlci1hZG1pbiIsImVtYWlsIjoicmhlcmlrc29uLm15QGdtYWlsLmNvbSIsImlhdCI6MTY1Mjc3NTYxM30.4Luu3E-Cjzgd59PIHGWg2Q2TrytlGJ0G4kkKItkfZ-Q",
+      },
+    });
+    setSpeakers(res.data.data);
+  };
 
   // Component DidMount
   React.useEffect(() => {
     getAllCategories();
   }, []);
 
+  React.useEffect(() => {
+    getAllSpeaker();
+  }, [keyword]);
+
   return (
     <div>
       <h2>Belajar React useEffect</h2>
 
+      <input type="text" value={keyword} name="keyword" onChange={(e) => setKeyword(e.target.value)} />
+
+      <h4>Data Categories</h4>
       <ul>
-        {categories.map((category) => {
-          return categories.name;
+        {/* Categories */}
+        {categories.map((category, i) => {
+          return <li key={i}>{category.name}</li>;
         })}
       </ul>
 
-      {/* <button onClick={() => setCategories("Frontend Programmer")}>Change Categories</button> */}
+      <h4>Data Speakers</h4>
+      <ul>
+        {/* Speakers */}
+        {speakers.map((speaker, i) => {
+          return <li key={i}>{speaker.name}</li>;
+        })}
+      </ul>
     </div>
 
     // ===================================================
